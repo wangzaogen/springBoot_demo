1f96928f9e16a7e603e86bbf6d65e4ab1bccbebc
“wangzaogen” <gavin_wangzg@163.com>
2022-04-28 19:52:44 +0800

    update code

--- elasticsearch-demo/src/main/java/com/example/demo/controller/CompanyController.java
+++ elasticsearch-demo/src/main/java/com/example/demo/controller/CompanyController.java
@@ -1,10 +1,13 @@
 package com.example.demo.controller;
 
 import com.example.demo.model.Company;
+import com.example.demo.repository.ESCompanyRepository;
 import com.example.demo.service.CompanyService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.elasticsearch.core.SearchHits;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.Optional;
 
 @RestController
 public class CompanyController {
@@ -12,8 +15,21 @@ public class CompanyController {
     @Autowired
     private CompanyService companyService;
 
+    @Autowired
+    private ESCompanyRepository esCompanyRepository;
+
     @PostMapping("/addCompany")
-    public String addBook(@RequestBody Company company){
+    public String addCompany(@RequestBody Company company){
+        companyService.addCompany(company);
+        return "ok";
+    }
+
+    @PostMapping("/updateCompany")
+    public String updateompany(@RequestBody Company company){
+        Optional<Company> co =  esCompanyRepository.findById(company.getId());
+        if (null == co.get()) {
+            return "company is not exist";
+        }
         companyService.addCompany(company);
         return "ok";
     }
